6dcfd0e3ac5c8bff5c2cd4c7451d58644d018cd1
Silabear <56885288+Silabear@users.noreply.github.com>
2023-03-05 13:00:25 +0000

    Fix authentication not working

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -3,6 +3,7 @@ import type { PageLoad } from './$types';
 export const load = (async ({ fetch }) => {
     const res = await fetch("https://api.datapackhub.net/user/me", {
         method: 'get',
+        credentials: 'include'
     })
     let data;
 
