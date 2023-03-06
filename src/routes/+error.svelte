3ecd3ff621007c552754961f189db69ef4d13ba8
Silabear <56885288+Silabear@users.noreply.github.com>
2023-03-06 18:54:15 +0000

    Update +error.svelte
    If this breaks the git history I will commit sad

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,5 +2,5 @@
   import { page } from '$app/stores';
 </script>
 
-<p>You've encountered an error</p>
+<p>Well, well, well. Looks like silly Cobblestone did an oopsie. Let's blame him and then make him fix the bug, even though it's very likely not their fault and more the fault of sillybear not adding cors to some dumb api route. Anyway, here is your probably undescriptive and useless error (it probably says Internal Error or smth dumb like that idfk):</p>
 <p>{$page.error?.message}</p>
