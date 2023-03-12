60701afe43aae0f9460eb1a2d12cdf4788a0b44e
Silabear <justsilas@yahoo.co.uk>
2023-03-12 15:43:10 +0000

    merge

--- src/components/ProfileComponent.svelte
+++ src/components/ProfileComponent.svelte
@@ -36,10 +36,6 @@
 
 <a href="/" target="_self" class="ml-6 flex items-center justify-center z-20">
   {#if $isAuthenticated}
-    <div>
-      <!-- <p class="text-right mr-3 w-18 dark:text-white font-brand">Silabear</p>
-			<p class="text-right mr-3 w-18 text-yellow-500 font-brand text-xs">⬤ Developer</p> -->
-    </div>
     <img
       src="../icons/bell.svg"
       width="32"
