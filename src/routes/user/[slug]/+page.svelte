efc48f955f18c6f227b216ce30f639d301e91686
Silabear <justsilas@yahoo.co.uk>
2023-03-12 15:02:57 +0000

    even more things

--- src/routes/user/[slug]/+page.svelte
+++ src/routes/user/[slug]/+page.svelte
@@ -51,7 +51,7 @@
       {#if $isAuthenticated && $userData.id === data.profile.id}
           <a
           href="#"
-          class="text-red-400 bg-red-400 bg-opacity-10 font-brand rounded-md px-2 md:px-3 py-1 md:py-2 text-md md:text-lg lg:text-xl hover:scale-105 transition-all border-2 border-red-400 active:brightness-75"
+          class="text-red-400 bg-red-400 bg-opacity-10 font-brand rounded-md px-2 md:px-3 py-2 md:py-2 text-md md:text-lg lg:text-xl hover:scale-105 transition-all border-2 border-red-400 active:brightness-75 mt-2"
         >
         <img
         src="../icons/settings.svg"
