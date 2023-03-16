3025f4a4056d1c05fdda866e800bf6e67923723f
boop-the-dog <aidenhperry@gmail.com>
2023-03-16 13:14:13 -0400

    more testing

--- src/routes/moderation/console/+page.svelte
+++ src/routes/moderation/console/+page.svelte
@@ -1,62 +1,53 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { isAuthenticated, userData, apiURL, postAuthed } from "$globals";
-  import { error } from "@sveltejs/kit";
 
   let isSmallWidth: boolean;
+  let cmd: string = "";
+  let submitCmd = () => {};
 
   onMount(() => {
-    if(!$isAuthenticated) throw error(401)
-
     console.log("User is " + $userData.username);
     // if($userData.role == "default"){
     //     window.location.replace("/")
     // }
     isSmallWidth = window.innerWidth < 768;
     addEventListener("resize", () => (isSmallWidth = window.innerWidth < 768));
-
-
     let inpu: HTMLInputElement = document.getElementById(
       "inpt"
     )! as HTMLInputElement;
     alert(inpu);
-    
+
     inpu.value = "your mother";
-    inpu.addEventListener("keydown", async (e) => {
-      let keyCode = e.key;
-      if (keyCode == "Enter") {
-        if (inpu.value != "") {
-          const cons = document.getElementById("cons")!;
-          let cmd = inpu.value;
-          cons.innerHTML = cons.innerHTML + `<li>⫻ ${cmd}</li>`;
-          inpu.value = "Loading...";
-          try {
-            await postAuthed(apiURL + "/moderation/console", {
-              command: cmd,
-            }).then((response) => {
-              response.text().then((text) => {
-                if (response.status == 200) {
-                  cons.innerHTML =
-                    cons.innerHTML + `<pre class='resp'>${text}</pre>`;
-                  inpu.value = "";
-                } else {
-                  cons.innerHTML =
-                    cons.innerHTML +
-                    `<li class='pb-4 text-red-500'>${text}</li>`;
-                  inpu.value = "";
-                }
-              });
+    submitCmd = async () => {
+      if (inpu.value != "") {
+        const cons = document.getElementById("cons")!;
+        cons.innerHTML = cons.innerHTML + `<li>⫻ ${cmd}</li>`;
+        inpu.value = "Loading...";
+        try {
+          await postAuthed(apiURL + "/moderation/console", {
+            command: cmd,
+          }).then((response) => {
+            response.text().then((text) => {
+              if (response.status == 200) {
+                cons.innerHTML =
+                  cons.innerHTML + `<pre class='resp'>${text}</pre>`;
+                inpu.value = "";
+              } else {
+                cons.innerHTML =
+                  cons.innerHTML + `<li class='pb-4 text-red-500'>${text}</li>`;
+                inpu.value = "";
+              }
             });
-          } catch (err) {
-            cons.innerHTML =
-              cons.innerHTML + `<li class='resp err'>${err}</li>`;
-            inpu.value = "";
-          }
-          let objDiv = document.getElementById("big")!;
-          objDiv.scrollTop = objDiv.scrollHeight;
+          });
+        } catch (err) {
+          cons.innerHTML = cons.innerHTML + `<li class='resp err'>${err}</li>`;
+          inpu.value = "";
         }
+        let objDiv = document.getElementById("big")!;
+        objDiv.scrollTop = objDiv.scrollHeight;
       }
-    });
+    };
   });
 </script>
 
@@ -79,7 +70,12 @@
           class="bottom-0 right-0 absolute bg-black p-3 w-full flex justify-around"
         >
           <p class="float-left">/</p>
-          <input on:submit|preventDefault id="inpt" class="w-full outline-none border-0 ml-1 bg-black" />
+          <input
+            on:submit|preventDefault={submitCmd}
+            bind:value={cmd}
+            id="inpt"
+            class="w-full outline-none border-0 ml-1 bg-black"
+          />
         </div>
       </div>
     {:else}
