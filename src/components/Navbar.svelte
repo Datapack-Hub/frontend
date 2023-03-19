89edafd141f314be979f3365addca0e3a1b8aeac
boop-the-dog <aidenhperry@gmail.com>
2023-03-19 13:36:59 -0400

    more changes

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { goto } from "$app/navigation";
   import { onMount } from "svelte";
   import ColorSchemeSelector from "./buttons/ColorSchemeSelector.svelte";
   import ProfileNavComponent from "./ProfileNavComponent.svelte";
@@ -11,7 +10,7 @@
   });
 </script>
 
-<nav class="w-full fixed {!isSmallWidth ? '' : 'bottom-0'} transition-all px-24 z-50">
+<nav class="w-full fixed {!isSmallWidth ? '' : 'bottom-0'} transition-all px-0 sm:px-8 md:px-16 lg:px-24 z-50">
   <div class="funky-gradient w-full">
     <div class="flex items-center mt-4 md:mt-0">
       <a
--- src/components/buttons/ButtonLink.svelte
+++ src/components/buttons/ButtonLink.svelte
@@ -5,7 +5,7 @@
 <button
 	on:click|preventDefault={clickEvent}
 	on:submit|preventDefault={null}
-	class="dark:text-newWhite text-black px-2 md:px-3 py-1 md:py-2 bg-dphOrange rounded-md font-bold text-md md:text-lg lg:text-xl font-brand hover:scale-110 transition-all active:brightness-75"
+	class="dark:text-newWhite text-black p-2 bg-dphOrange rounded-md font-bold text-md md:text-lg lg:text-xl font-brand hover:scale-110 transition-all active:brightness-75"
 >
 	<slot />
 </button>
