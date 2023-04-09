b87714e72a51733ea7863976cf52f12ec1016366
boop-the-dog <aidenhperry@gmail.com>
2023-04-09 11:42:43 -0400

    how did that happen

--- src/routes/user/[slug]/+page.svelte
+++ src/routes/user/[slug]/+page.svelte
@@ -76,21 +76,6 @@
       >
         {data.profile?.bio.replaceAll("\\n", "\n")}
       </p>
-      {#if $isAuthenticated && $userData.id === data.profile?.id}
-        <a
-          href="edit"
-          class="text-red-400 bg-red-400 bg-opacity-10 font-brand rounded-md px-2 md:px-3 py-2 md:py-2 text-md md:text-lg lg:text-xl hover:scale-105 transition-all border-2 border-red-400 active:brightness-75 mt-2"
-        >
-          <img
-            src="../icons/settings.svg"
-            alt="settings"
-            width="24"
-            height="24"
-            class="float-left invert mr-2 max-w-sm stroke-red-400"
-          />
-          Profile Settings
-        </a>
-      {/if}
     </div>
     <div
       class="w-full mx-24 h-full overflow-auto md:overflow-y-auto mt-16 md:mt-0 styled-scrollbar"
