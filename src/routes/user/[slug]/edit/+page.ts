ce30fd813fd27cdec59eb1b840ad1041f5a1a06d
Silabear <56885288+Silabear@users.noreply.github.com>
2023-04-11 18:37:52 +0000

    edit returns 403 if not user or staff

--- src/routes/user/[slug]/edit/+page.ts
+++ src/routes/user/[slug]/edit/+page.ts
@@ -1,14 +1,22 @@
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
-import { apiURL, fetchAuthed } from '$globals';
+import { apiURL, fetchAuthed, userData } from '$globals';
 import { browser } from '$app/environment';
 
 export const load = (async ({ params, fetch }) => {
     if(browser){
         const user = await fetchAuthed("get", apiURL +"/user/" + params.slug)
-        if(user.ok){
+        const me = await fetchAuthed("get", apiURL + "/user/me")
+
+        const userJSON = await user.json() as User
+        const meJSON = await me.json() as User
+        if(user.ok && me.ok){
+            if (userJSON.username != meJSON.username && ["admin","moderator"].includes(meJSON.role)) {
+                throw error(403, {message: 'Not allowed!',description: "This is not you, you can't edit their profile."}) 
+            }
+
             return {
-                profile: await user.json() as User
+                profile: userJSON
             }
         }
     }
