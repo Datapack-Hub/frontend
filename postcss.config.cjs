9dc5c0197d08d605fb1dc36f280675f7fb82e995
boop-the-dog <aidenhperry@gmail.com>
2023-04-24 19:29:08 -0400

    minor optimization

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,8 +3,7 @@ const autoprefixer = require("autoprefixer");
 const cssnano = require('cssnano')
 
 const config = {
-  plugins: [//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
-    //But others, like autoprefixer, need to run after,
+  plugins: [
     tailwindcss(), //Some plugins, like tailwindcss/nesting, need to run before Tailwind, tailwindcss(), //But others, like autoprefixer, need to run after, 
     autoprefixer(),
     cssnano()
