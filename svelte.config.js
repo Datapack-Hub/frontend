138199cb55fc5245285beeb8f5d8d4b830c4db10
boop-the-dog <aidenhperry@gmail.com>
2023-04-29 09:09:04 -0400

    no csp, too hard :(

--- svelte.config.js
+++ svelte.config.js
@@ -19,39 +19,6 @@ const config = {
       $components: "./src/components/",
       $globals: "./src/routes/globals.ts",
     },
-    csp: {
-      mode: "hash",
-      directives: {
-        'base-uri': ["'self'"],
-        'child-src': ["'self'"],
-        'connect-src': ["'self'", 'ws://localhost:*'],
-        'img-src': ["'self'", '*'],
-        'font-src': [
-          "'self'",
-          'https://*.google.com',
-          'https://*.googleapis.com',
-          'https://*.gstatic.com'
-        ],
-        'form-action': ["'self'"],
-        'frame-ancestors': ["'self'"],
-        'manifest-src': ["'self'"],
-        'media-src': ["'self'", 'datapackhub.net', '*.datapackhub.net', 'youtube.com', 'youtu.be'],
-        'object-src': ["'none'"],
-        'style-src': ["'self'", "'unsafe-inline'"],
-        'default-src': [
-          'self',
-          '*.datapackhub.net',
-          'static.cloudflareinsights.com',
-          'https://*.google.com',
-          'https://*.googleapis.com',
-          'https://*.gstatic.com',
-        ],
-        'script-src': [
-          'self',
-        ],
-        'worker-src': ["'self'"],
-      },
-    }
   },
 };
 
