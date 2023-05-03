28e65db0d43b78c882ede1f5759a1bd117380c69
boop-the-dog <aidenhperry@gmail.com>
2023-05-03 17:29:57 -0400

    start of footer redesign

--- src/components/nav/Footer.svelte
+++ src/components/nav/Footer.svelte
@@ -1,8 +1,20 @@
 <footer
-  class="flex h-64 border-t-2 border-new-white-100 bg-new-white-200 px-8 dark:border-stone-700 dark:bg-stone-900 md:px-16 lg:px-24"
+  class="flex items-center justify-between h-64 border-t-2 border-new-white-100 bg-new-white-200 px-8 dark:border-stone-700 dark:bg-stone-900 md:px-16 lg:px-24"
   id="footer">
-  <img
-    src="/logos/dph.svg"
-    alt="logo"
-    class="min-h-8 transition-all hover:brightness-75" />
+  <div class="flex items-center">
+    <img
+      src="/logos/dph.svg"
+      alt="logo"
+      class="h-12 transition-all hover:brightness-75" />
+    <span
+      class="hidden font-brand text-2xl font-bold text-black transition-colors hover:text-neutral-700 active:text-neutral-600 dark:text-white dark:hover:text-neutral-400 dark:active:text-neutral-500 md:inline-block">
+      Datapack Hub
+    </span>
+  </div>
+  <div></div>
+  <div>
+    <!-- put header content here -->
+    <p class="dark:text-white">Content coming soon!</p>
+  </div>
+  <div></div>
 </footer>
