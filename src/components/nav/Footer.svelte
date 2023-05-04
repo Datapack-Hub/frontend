07b09772bdf40e3276dba812879fbae17be0be83
Silabear <justsilas@yahoo.co.uk>
2023-05-04 18:40:50 +0100

    added things to the footer

--- src/components/nav/Footer.svelte
+++ src/components/nav/Footer.svelte
@@ -6,6 +6,7 @@
   class="flex h-96 flex-col items-center justify-evenly border-t-2 border-new-white-100 bg-new-white-200 px-8 dark:border-stone-700 dark:bg-stone-900 md:h-64 md:flex-row md:justify-between md:px-16 lg:px-24"
   id="footer">
   <div>
+    <div class="flex space-x-2 items-center">
     <img
       src="/logos/dph.svg"
       alt="logo"
@@ -14,44 +15,43 @@
       class="w-full text-center font-brand text-2xl font-bold transition-colors hover:text-neutral-700 active:text-neutral-600 dark:text-white dark:hover:text-neutral-400 dark:active:text-neutral-500 md:text-left">
       Datapack Hub
     </p>
+  </div>
     <p
       class="mt-4 font-brand text-sm font-light text-opacity-25 dark:text-white dark:text-opacity-25">
       Copyright © 2023 Datapack Hub
     </p>
   </div>
   <div></div>
-  <div class="w-full max-w-5xl md:w-2/3">
+  <div class="w-full md:w-2/3">
     <div class="flex w-full">
+      <h3
+        class="w-1/3 text-center font-brand text-xl font-bold text-orange-500 md:text-right">
+        Support
+      </h3>
       <h3
         class="w-1/3 text-center font-brand text-xl font-bold text-orange-500 md:text-right">
         Socials
       </h3>
       <h3
         class="w-1/3 text-center font-brand text-xl font-bold text-orange-500 md:text-right">
-        Info
-      </h3>
-      <h3
-        class="w-1/3 text-center font-brand text-xl font-bold text-orange-500 md:text-right">
-        idk
+        Information
       </h3>
     </div>
-    <CasualLine />
     <!-- put header content here -->
-    <div class="flex w-full">
+    <div class="flex w-full text-center">
       <ul class="w-1/3 text-center font-brand dark:text-white md:text-right">
-        <li>Thing</li>
-        <li>Thing</li>
-        <li>Thing</li>
+        <li><a href="https://discord.gg/aEXsdjjdu4">Community Discord</a></li>
+        <li><a href="/staff">Staff Team</a></li>
       </ul>
       <ul class="w-1/3 text-center font-brand dark:text-white md:text-right">
-        <li>Thing</li>
-        <li>Thing</li>
-        <li>Thing</li>
+        <li><a href="https://discord.gg/aEXsdjjdu4">Discord</a></li>
+        <li><a href="https://twitter.com/DatapackHub">Twitter</a></li>
+        <li><a href="https://github.com/Datapack-Hub">Community Discord</a></li>
       </ul>
       <ul class="w-1/3 text-center font-brand dark:text-white md:text-right">
-        <li>Thing</li>
-        <li>Thing</li>
-        <li>Thing</li>
+        <li><a href="/rules">Site Rules</a></li>
+        <li><a href="/privacy">Privacy Policy</a></li>
+        <li><a href="/terms">Terms and Conditions</a></li>
       </ul>
     </div>
   </div>
