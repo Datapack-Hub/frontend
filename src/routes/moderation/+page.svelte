5e8161ab0e004ab249c3c8e0a57d36075d9ea2dd
boop-the-dog <aidenhperry@gmail.com>
2023-05-05 14:02:27 -0400

    improved format performance

--- src/routes/moderation/+page.svelte
+++ src/routes/moderation/+page.svelte
@@ -1,43 +1,43 @@
 <script lang="ts">
-import { apiURL, fetchAuthed, userData } from "$globals";
-import { goto } from "$app/navigation";
+  import { apiURL, fetchAuthed, userData } from "$globals";
+  import { goto } from "$app/navigation";
   import { browser } from "$app/environment";
   import { load } from "../notifications/+page";
 
-let rolesJson: Role[];
-let activePage = "dashboard"
+  let rolesJson: Role[];
+  let activePage = "dashboard";
 
-function titleCase(str: string | undefined): string {
+  function titleCase(str: string | undefined): string {
     if (str == undefined) return "null";
     return str
-    .toLowerCase()
-    .split(" ")
-    .map((word) => {
+      .toLowerCase()
+      .split(" ")
+      .map((word) => {
         return word.charAt(0).toUpperCase() + word.slice(1);
-    })
-    .join(" ");
-}
+      })
+      .join(" ");
+  }
 
-async function loadRoles() {
+  async function loadRoles() {
     if ($userData.role == "default") {
-        goto("/");  
+      goto("/");
     }
 
     let logs = await fetchAuthed("get", `${apiURL}/user/staff/roles`);
     rolesJson = (await logs.json())["roles"];
-}
+  }
 
-if(browser){
-    loadRoles()
-}
+  if (browser) {
+    loadRoles();
+  }
 </script>
 
 <svelte:head>
-    <title>Moderation | Datapack Hub</title>
+  <title>Moderation | Datapack Hub</title>
 </svelte:head>
 
 <main
-  class="bg-new-white-200 px-4 transition-all dark:bg-stone-900 lg:px-32 xl:px-64 -translate-y-20 md:translate-y-0">
+  class="-translate-y-20 bg-new-white-200 px-4 transition-all dark:bg-stone-900 md:translate-y-0 lg:px-32 xl:px-64">
   <div
     class=" h-screen w-full flex-col items-center md:flex-row md:items-start md:pt-20">
     <h1
@@ -45,53 +45,62 @@ if(browser){
       Moderation Dashboard
     </h1>
     <div class="mb-2 flex space-x-2">
-        <button class="rounded-xl {activePage === 'dashboard' ? 'bg-stone-600' : 'bg-stone-800'} hover:scale-102 p-1 px-3 font-brand dark:text-white cursor-pointer" on:click={() => activePage = 'dashboard'}
-        >Dashboard</button>
-        <button class="rounded-xl {activePage === 'roles' ? 'bg-stone-600' : 'bg-stone-800'} hover:scale-102 p-1 px-3 font-brand dark:text-white cursor-pointer" on:click={() => activePage = 'roles'}
-        >Roles</button>
-        <a href="/moderation/console"><button class="rounded-xl bg-stone-800 hover:scale-102 p-1 px-3 font-brand dark:text-white cursor-pointer"
-        >Console</button></a>
+      <button
+        class="rounded-xl {activePage === 'dashboard'
+          ? 'bg-stone-600'
+          : 'bg-stone-800'} cursor-pointer p-1 px-3 font-brand hover:scale-102 dark:text-white"
+        on:click="{() => (activePage = 'dashboard')}">Dashboard</button>
+      <button
+        class="rounded-xl {activePage === 'roles'
+          ? 'bg-stone-600'
+          : 'bg-stone-800'} cursor-pointer p-1 px-3 font-brand hover:scale-102 dark:text-white"
+        on:click="{() => (activePage = 'roles')}">Roles</button>
+      <a href="/moderation/console"
+        ><button
+          class="cursor-pointer rounded-xl bg-stone-800 p-1 px-3 font-brand hover:scale-102 dark:text-white"
+          >Console</button
+        ></a>
     </div>
 
     {#if activePage == "dashboard"}
-    <div class="text-center align-middle md:text-start">
-      <div class="flex rounded-xl bg-stone-800 p-2 py-3 space-x-2">
-        <label for="icon" class="max-w-100">
-          <span
-            class="p-2 bg-green-600 font-brand font-bold rounded-xl dark:text-white cursor-pointer"
-            >Upload datapack ZIP</span>
-        </label>
-        <span class="align-center dark:text-white font-brand">(Supported: *.zip)</span>
-        <!-- <p class="align-middle font-brand dark:text-new-white-200">No versions yet!</p> -->
-        
+      <div class="text-center align-middle md:text-start">
+        <div class="flex space-x-2 rounded-xl bg-stone-800 p-2 py-3">
+          <label for="icon" class="max-w-100">
+            <span
+              class="cursor-pointer rounded-xl bg-green-600 p-2 font-brand font-bold dark:text-white"
+              >Upload datapack ZIP</span>
+          </label>
+          <span class="align-center font-brand dark:text-white"
+            >(Supported: *.zip)</span>
+          <!-- <p class="align-middle font-brand dark:text-new-white-200">No versions yet!</p> -->
+        </div>
       </div>
-    </div>
-
     {:else if activePage == "roles"}
-    <div class="text-center align-middle md:text-start flex flex-col rounded-xl bg-stone-800 p-2 py-3 space-x-2">
+      <div
+        class="flex flex-col space-x-2 rounded-xl bg-stone-800 p-2 py-3 text-center align-middle md:text-start">
         <h1
-            class="m-2 text-center font-brand text-2xl font-bold dark:text-white md:text-start">
-            Site Roles
+          class="m-2 text-center font-brand text-2xl font-bold dark:text-white md:text-start">
+          Site Roles
         </h1>
-        <table class="table-auto p-2 text-left font-brand rounded-xl">
-            <tr class="bg-emerald-500 p-2">
-                <th class="p-2 dark:text-white">Role Name</th>
-                <th class="p-2 dark:text-white">Permissions</th>
-            </tr>
-            {#each rolesJson ?? [] as i}
+        <table class="table-auto rounded-xl p-2 text-left font-brand">
+          <tr class="bg-emerald-500 p-2">
+            <th class="p-2 dark:text-white">Role Name</th>
+            <th class="p-2 dark:text-white">Permissions</th>
+          </tr>
+          {#each rolesJson ?? [] as i}
             <tr class="odd:bg-stone-700/25">
-                <td
+              <td
                 ><p style="color: {i.color};" class="dark:text-white">
-                    ⬤ {titleCase(i.name)}
+                  ⬤ {titleCase(i.name)}
                 </p></td>
-                <td class="dark:text-white"
+              <td class="dark:text-white"
                 >{#if i.permissions.length != 0}{i.permissions.join(
                     " | "
-                    )}{:else}None{/if}</td>
+                  )}{:else}None{/if}</td>
             </tr>
-            {/each}
+          {/each}
         </table>
-    </div>
+      </div>
     {/if}
   </div>
 </main>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,51 +3,68 @@
   import ProjectComponent from "$components/ProjectComponent.svelte";
   import { apiURL } from "$globals";
   import type { PageData } from "./$types";
-
-  import IconSearch from '~icons/tabler/Search.svelte'
-
-  let query: string
-
-  async function search() {
-    let searchResult = await fetch(apiURL + `/projects/search?query=${query}`)
-    data.projects = (await searchResult.json()).result as Project[]
-  }
+  import IconSearch from "~icons/tabler/Search.svelte";
 
   export let data: PageData;
+  let query: string;
+
+  async function search() {
+    let searchResult = await fetch(apiURL + `/projects/search?query=${query}`);
+    data.projects = (await searchResult.json()).result as Project[];
+  }
 </script>
 
 <svelte:head>
   <title>Projects | Datapack Hub</title>
 </svelte:head>
 
-<main
-  class="px-8 md:px-16 lg:px-24">
+<main class="px-8 md:px-16 lg:px-24">
   <div
-    class="items-center pt-0 md:pt-20 -translate-y-20 md:translate-y-0 md:flex-row md:items-start">
+    class="-translate-y-20 items-center pt-0 md:translate-y-0 md:flex-row md:items-start md:pt-20">
     <h1
       class="mt-8 pb-2 text-center font-brand text-5xl font-bold dark:text-white md:text-start md:text-4xl lg:text-5xl">
       Browse Datapacks
     </h1>
-    <div class="flex my-4">
-      <div class="bg-new-white-300 dark:bg-stone-600 rounded-full px-2 py-1 w-64 flex items-center focus-within:outline focus-within:outline-2 focus-within:outline-orange-500">
-        <IconSearch color="white" on:click={search}/>
-        <input placeholder="Search Datapacks" type="text" bind:value="{query}" id="query" class="bg-new-white-300 dark:bg-stone-600 ml-2 dark:text-white focus:outline-none placeholder:text-stone-400 font-brand" on:input="{search}">
+    <div class="my-4 flex">
+      <div
+        class="flex w-64 items-center rounded-full bg-new-white-300 px-2 py-1 focus-within:outline focus-within:outline-2 focus-within:outline-orange-500 dark:bg-stone-600">
+        <IconSearch color="white" on:click="{search}" />
+        <input
+          placeholder="Search Datapacks"
+          type="text"
+          bind:value="{query}"
+          id="query"
+          class="ml-2 bg-new-white-300 font-brand placeholder:text-stone-400 focus:outline-none dark:bg-stone-600 dark:text-white"
+          on:input="{search}" />
       </div>
       <div class="px-2"></div>
       <div class="flex items-center">
-        <a href="/projects" class="px-1 dark:text-white bg-orange-600 mx-1 rounded-full w-6 h-6 text-center">1</a>
-        <a href="/projects" class="px-1 dark:text-white bg-orange-600 mx-1 rounded-full w-6 h-6 text-center">2</a>
-        <a href="/projects" class="px-1 dark:text-white bg-orange-600 mx-1 rounded-full w-6 h-6 text-center">3</a>
-        <a href="/projects" class="px-1 dark:text-white bg-orange-600 mx-1 rounded-full w-6 h-6 text-center">4</a>
-        <a href="/projects" class="px-1 dark:text-white bg-orange-600 mx-1 rounded-full w-6 h-6 text-center">5</a>
+        <a
+          href="/projects"
+          class="mx-1 h-6 w-6 rounded-full bg-orange-600 px-1 text-center dark:text-white"
+          >1</a>
+        <a
+          href="/projects"
+          class="mx-1 h-6 w-6 rounded-full bg-orange-600 px-1 text-center dark:text-white"
+          >2</a>
+        <a
+          href="/projects"
+          class="mx-1 h-6 w-6 rounded-full bg-orange-600 px-1 text-center dark:text-white"
+          >3</a>
+        <a
+          href="/projects"
+          class="mx-1 h-6 w-6 rounded-full bg-orange-600 px-1 text-center dark:text-white"
+          >4</a>
+        <a
+          href="/projects"
+          class="mx-1 h-6 w-6 rounded-full bg-orange-600 px-1 text-center dark:text-white"
+          >5</a>
       </div>
     </div>
     <CasualLine />
     <div class="mt-4 flex">
-      <div class="w-1/5 bg-stone-800 rounded-xl">
-        
-      </div>
-      <div class="w-full mx-3">
+      <div class="w-1/5 rounded-xl bg-stone-800"></div>
+      <div class="mx-3 w-full">
         {#if data.projects}
           {#each data.projects as project}
             <ProjectComponent project="{project}" />
