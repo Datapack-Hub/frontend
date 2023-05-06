2c6a8de3bf84b5ec637988d0d4c812d5bf37771c
boop-the-dog <aidenhperry@gmail.com>
2023-05-06 16:25:57 -0400

    fix weird button width

--- src/routes/project/[slug]/+page.svelte
+++ src/routes/project/[slug]/+page.svelte
@@ -62,7 +62,7 @@
     </div>
     <a
       href="/download"
-      class="button-style"
+      class="button-style h-fit"
       >Download Latest</a>
   </div>
   <div class="my-2 mt-4 flex space-x-2">
