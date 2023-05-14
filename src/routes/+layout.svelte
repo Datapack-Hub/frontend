5cabf78b6aeaf4fcac99480a811391140eb212ce
Cobblestone <aidenhperry@gmail.com>
2023-05-14 09:45:16 -0400

    it broke

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -53,10 +53,6 @@
   setContext("roleData", role);
 </script>
 
-<svelte:head>
-  <title>{document.title ?? "Datapack Hub"}</title>
-</svelte:head>
-
 <!-- {#await pageLoad() then} -->
 {#if $isAuthenticated}
   {#if ["helper", "moderator", "admin"].includes($user.role)}
