327e13d54adee705c09cfb83be275152c50c118f
boop-the-dog <aidenhperry@gmail.com>
2023-05-21 09:23:45 -0400

    attempt to force blur to use gpu

--- src/lib/components/modals/BannedModal.svelte
+++ src/lib/components/modals/BannedModal.svelte
@@ -37,7 +37,7 @@
 
 {#if $isAuthenticated && visible}
   <div
-    class="fixed left-0 top-0 z-50 flex h-screen w-screen backdrop-blur-lg backdrop-brightness-50">
+    class="fixed left-0 top-0 z-50 flex h-screen w-screen backdrop-blur-lg transform-gpu backdrop-brightness-50">
     <div
       class="relative m-auto w-1/2 justify-self-center rounded-xl p-3 align-middle dark:bg-stone-800">
       <a href="/" class="z-20 flex cursor-pointer items-center justify-center">
