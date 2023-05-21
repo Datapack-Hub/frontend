f41a6622a1d7ddaacae8522430fc580b2782c01c
boop-the-dog <aidenhperry@gmail.com>
2023-05-21 17:27:36 -0400

    this actually seems like a logical fix, hope it works

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,43 +25,45 @@
     </div>
     <Footer />
     <Toaster />
-  </div>
-{:else}
-  <div class="fixed left-0 top-0 flex h-screen w-screen bg-stone-900">
-    <div
-      class="relative m-auto w-1/3 justify-self-center rounded-xl bg-stone-800 p-3 align-middle shadow-md shadow-black/25">
-      <div class="flex items-center justify-center">
-        <img
-          src="/logos/dph.svg"
-          alt="logo"
-          class="mr-1 h-8 rounded-full transition-all hover:brightness-75"
-          height="32"
-          width="32" />
-        <span
-          class="hidden font-brand text-2xl font-bold text-white transition-colors md:block">
-          Datapack Hub
-        </span>
+  {:else}
+    <div class="fixed left-0 top-0 flex h-screen w-screen bg-stone-900">
+      <div
+        class="relative m-auto w-1/3 justify-self-center rounded-xl bg-stone-800 p-3 align-middle shadow-md shadow-black/25">
+        <div class="flex items-center justify-center">
+          <img
+            src="/logos/dph.svg"
+            alt="logo"
+            class="mr-1 h-8 rounded-full transition-all hover:brightness-75"
+            height="32"
+            width="32" />
+          <span
+            class="hidden font-brand text-2xl font-bold text-white transition-colors md:block">
+            Datapack Hub
+          </span>
+        </div>
+        <CasualLine />
+        <p class="mb-5 font-brand text-lg text-white">
+          The Datapack Hub website is coming very soon!
+        </p>
+        <p class="font-brand text-white"><b>In the meantime, come chat!</b></p>
+        <p>
+          <a
+            href="https://discord.gg/aEXsdjjdu4"
+            class="font-brand text-sky-200">
+            - Join our Discord</a>
+        </p>
+        <p class="mb-5">
+          <a
+            href="https://planetminecraft.com/group/daily_datapackers"
+            class="font-brand text-sky-200">
+            - Follow our PMC group!</a>
+        </p>
+        <p class="font-brand text-sm font-thin italic text-white">
+          ~ your favourite datapack hub staff team
+        </p>
       </div>
-      <CasualLine />
-      <p class="mb-5 font-brand text-lg text-white">
-        The Datapack Hub website is coming very soon!
-      </p>
-      <p class="font-brand text-white"><b>In the meantime, come chat!</b></p>
-      <p>
-        <a href="https://discord.gg/aEXsdjjdu4" class="font-brand text-sky-200">
-          - Join our Discord</a>
-      </p>
-      <p class="mb-5">
-        <a
-          href="https://planetminecraft.com/group/daily_datapackers"
-          class="font-brand text-sky-200">
-          - Follow our PMC group!</a>
-      </p>
-      <p class="font-brand text-sm font-thin italic text-white">
-        ~ your favourite datapack hub staff team
-      </p>
     </div>
-    {/if}
-  </div>
+  {/if}
+</div>
 
 <!-- {/await} -->
