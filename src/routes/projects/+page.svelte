f72cd75d43887f06db832f418475469275891501
boop-the-dog <aidenhperry@gmail.com>
2023-05-23 16:01:38 -0400

    i swear

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -7,13 +7,14 @@
   import autoAnimate from "@formkit/auto-animate";
 
   import IconSearch from "~icons/tabler/Search.svelte";
+  import { browser } from "$app/environment";
 
   let query: string;
 
-  let search = debounce(async () => {
+  let search = browser ? debounce(async () => {
     let searchResult = await fetch(apiURL + `/projects/search?query=${query}`);
     data.projects = (await searchResult.json()).result as Project[];
-  }, 300);
+  }, 300) : () => {};
 
   export let data: PageData;
 </script>
