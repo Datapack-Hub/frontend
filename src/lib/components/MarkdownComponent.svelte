a98eba3b19ed73bef76e7bbb891aaf15c1498b70
boop-the-dog <aidenhperry@gmail.com>
2023-05-24 13:18:35 -0400

    better markdown?

--- src/lib/components/MarkdownComponent.svelte
+++ src/lib/components/MarkdownComponent.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
-  import SvelteMarkdown from "svelte-markdown";
   import DOMPurify from "isomorphic-dompurify";
+  import { marked } from "marked";
 
   export let source = "";
 
-  $: cleanedSource = DOMPurify.sanitize(source, {
+  $: html = marked.parse(DOMPurify.sanitize(source, {
     FORBID_ATTR: ["style", "class", "placeholder", "src"],
     FORBID_TAGS: ["canvas", "svg", "iframe", "img"],
     ALLOWED_TAGS: ["details", "summary"]
-  });
+  }), {
+    async: true,
+    breaks: true,
+    gfm: true
+  })
 </script>
 
 <div class="prose prose-stone leading-snug dark:prose-invert">
-  <SvelteMarkdown source="{cleanedSource}" />
+  {#await html then out}
+    {@html out}
+  {/await}
 </div>
