b99ef7c788ba2b90f6fcf3b616c3dbd2f3035d94
Silabear <justsilas@yahoo.co.uk>
2023-05-26 15:45:44 +0100

    better modal

--- src/lib/components/modals/Modal.svelte
+++ src/lib/components/modals/Modal.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { fly } from "svelte/transition";
+  import IconCross from "~icons/tabler/X.svelte";
 
   let visible = false;
   export function open() {
@@ -18,8 +19,8 @@
       in:fly="{{ y: 300 }}"
       class="relative m-auto w-1/3 justify-self-center rounded-xl p-3 align-middle dark:bg-stone-800">
       <button
-        class="float-right cursor-pointer select-none font-black text-pearl-lusta-950 dark:text-white"
-        on:click="{() => close()}">X</button>
+        class="float-right cursor-pointer select-none font-black text-pearl-lusta-950 dark:text-white my-1"
+        on:click="{() => close()}"><IconCross /></button>
       <slot />
     </div>
   </div>
