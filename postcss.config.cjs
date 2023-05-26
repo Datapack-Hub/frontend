47af72e3b07446e822e7d1ff0112283f46adbcd6
Silabear <justsilas@yahoo.co.uk>
2023-05-26 16:52:19 +0100

    actually removed tailwind

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,8 @@
-const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
 const cssnano = require('cssnano')
 
 const config = {
   plugins: [
-    tailwindcss(), //Some plugins, like tailwindcss/nesting, need to run before Tailwind, tailwindcss(),
     cssnano({ preset: "advanced", plugins: [autoprefixer] })
   ],
 };
