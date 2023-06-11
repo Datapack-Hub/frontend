6882e87206fad92f5db1b46912013c83bd7f7ffe
boop-the-dog <aidenhperry@gmail.com>
2023-06-11 13:50:49 -0400

    how did that even happen

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -27,63 +27,60 @@
   <div
     class="my-4 flex flex-col items-center justify-center sm:flex-row md:justify-normal space-x-2">
     <div
-      class="my-4 flex flex-col items-center justify-center sm:flex-row md:justify-normal space-x-2">
-      <div
-        class="flex w-64 items-center rounded-full bg-pearl-lusta-200 px-2 py-1 focus-within:outline focus-within:outline-2 focus-within:outline-orange-500 dark:bg-stone-700">
-        <IconSearch color="white" on:click="{search}" />
-        <input
-          placeholder="Search Datapacks..."
-          type="text"
-          bind:value="{query}"
-          id="query"
-          maxlength="75"
-          class="ml-2 bg-pearl-lusta-200 text-pearl-lusta-950 placeholder:text-stone-400 focus:outline-none dark:bg-stone-700 dark:text-white"
-          on:input="{search}" />
-      </div>
-      <div class="flex items-center">
-        <a
-          href="/projects"
-          class="mx-1 mt-4 h-8 w-8 rounded-full bg-dph-orange p-1 text-center font-bold text-pearl-lusta-950 dark:text-white sm:mt-0"
-          >1</a>
-        <a
-          href="/projects"
-          class="mx-1 mt-4 h-8 w-8 rounded-full bg-dph-orange p-1 text-center font-bold text-pearl-lusta-950 dark:text-white sm:mt-0"
-          >2</a>
-        <a
-          href="/projects"
-          class="mx-1 mt-4 h-8 w-8 rounded-full bg-dph-orange p-1 text-center font-bold text-pearl-lusta-950 dark:text-white sm:mt-0"
-          >3</a>
-        <a
-          href="/projects"
-          class="mx-1 mt-4 h-8 w-8 rounded-full bg-dph-orange p-1 text-center font-bold text-pearl-lusta-950 dark:text-white sm:mt-0"
-          >4</a>
-        <a
-          href="/projects"
-          class="mx-1 mt-4 h-8 w-8 rounded-full bg-dph-orange p-1 text-center font-bold text-pearl-lusta-950 dark:text-white sm:mt-0"
-          >5</a>
-      </div>
+      class="flex w-64 items-center rounded-full bg-pearl-lusta-200 px-2 py-1 focus-within:outline focus-within:outline-2 focus-within:outline-orange-500 dark:bg-stone-700">
+      <IconSearch color="white" on:click="{search}" />
+      <input
+        placeholder="Search Datapacks..."
+        type="text"
+        bind:value="{query}"
+        id="query"
+        maxlength="75"
+        class="ml-2 bg-pearl-lusta-200 text-pearl-lusta-950 placeholder:text-stone-400 focus:outline-none dark:bg-stone-700 dark:text-white"
+        on:input="{search}" />
     </div>
-    <CasualLine />
-    <div class="mt-4 flex flex-col md:flex-row">
-      <div
-        class="mb-3 md:mb-0 w-full md:w-1/2 rounded-xl bg-stone-800 p-2 lg:w-1/4 xl:w-1/5">
-        <p class="w-full text-center text-xl text-white">Search Settings</p>
-        <p class="w-full text-center text-white/60">TODO: Add these</p>
-      </div>
-      {#if !data.projects || !(data.projects.length >= 1)}
-        <h2 class=" text-pearl-lusta-950 dark:text-white">No results found</h2>
-      {:else}
-        <ul class="ml-0 w-full space-y-2 md:ml-3" use:autoAnimate>
-          {#each data.projects as project}
-            <!-- uncomment these to see the featured stuff on the thingy thing -->
-            <!-- {#if Math.random() < 0.2} -->
-            <!-- <FeaturedProjectComponent project="{project}" /> -->
-            <!-- {:else} -->
-            <ProjectComponent project="{project}" />
-            <!-- {/if} -->
-          {/each}
-        </ul>
-      {/if}
+    <div class="flex items-center">
+      <a
+        href="/projects"
+        class="mx-1 mt-4 h-8 w-8 rounded-full bg-dph-orange p-1 text-center font-bold text-pearl-lusta-950 dark:text-white sm:mt-0"
+        >1</a>
+      <a
+        href="/projects"
+        class="mx-1 mt-4 h-8 w-8 rounded-full bg-dph-orange p-1 text-center font-bold text-pearl-lusta-950 dark:text-white sm:mt-0"
+        >2</a>
+      <a
+        href="/projects"
+        class="mx-1 mt-4 h-8 w-8 rounded-full bg-dph-orange p-1 text-center font-bold text-pearl-lusta-950 dark:text-white sm:mt-0"
+        >3</a>
+      <a
+        href="/projects"
+        class="mx-1 mt-4 h-8 w-8 rounded-full bg-dph-orange p-1 text-center font-bold text-pearl-lusta-950 dark:text-white sm:mt-0"
+        >4</a>
+      <a
+        href="/projects"
+        class="mx-1 mt-4 h-8 w-8 rounded-full bg-dph-orange p-1 text-center font-bold text-pearl-lusta-950 dark:text-white sm:mt-0"
+        >5</a>
     </div>
+  </div>
+  <CasualLine />
+  <div class="mt-4 flex flex-col md:flex-row">
+    <div
+      class="mb-3 md:mb-0 w-full md:w-1/2 rounded-xl bg-stone-800 p-2 lg:w-1/4 xl:w-1/5">
+      <p class="w-full text-center text-xl text-white">Search Settings</p>
+      <p class="w-full text-center text-white/60">TODO: Add these</p>
+    </div>
+    {#if !data.projects || !(data.projects.length >= 1)}
+      <h2 class=" text-pearl-lusta-950 dark:text-white">No results found</h2>
+    {:else}
+      <ul class="ml-0 w-full space-y-2 md:ml-3" use:autoAnimate>
+        {#each data.projects as project}
+          <!-- uncomment these to see the featured stuff on the thingy thing -->
+          <!-- {#if Math.random() < 0.2} -->
+          <!-- <FeaturedProjectComponent project="{project}" /> -->
+          <!-- {:else} -->
+          <ProjectComponent project="{project}" />
+          <!-- {/if} -->
+        {/each}
+      </ul>
+    {/if}
   </div>
 </main>
