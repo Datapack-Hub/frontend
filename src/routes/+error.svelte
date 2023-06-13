1a382a115e7b5dc1c5681348269bf8dfa5539622
boop-the-dog <aidenhperry@gmail.com>
2023-06-13 15:30:46 -0400

    readded the error messages

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,6 +2,34 @@
   import { page } from "$app/stores";
 
   let message = $page.error?.description;
+
+  if (!message) {
+    switch ($page.status) {
+      case 404:
+        message =
+          "Looks like you hit a block in your adventure, but don't worry, we've got plenty of datapacks to help you break through. Unfortunately, this page isn't one of them";
+        break;
+
+      case 401:
+        message =
+          'You are logged out, please log back in to enjoy all the luxuries we supply';
+        break;
+
+      case 403:
+        message =
+          "Forbidden fruit! This page is tempting, but unfortunately, it's off-limits for now. Don't worry, we have plenty of other juicy datapacks for you to enjoy!";
+        break;
+
+      case 500:
+        message =
+          "You have stumbled across the eternal error. Please don't report this to the staff team, we don't know how to fix this either.";
+        break;
+
+      default:
+        message =
+          "We haven't coded a message for this error yet! If you think we should, reach out to us, or, if you're a dev, make a pull request.";
+    }
+  }
 </script>
 
 <svelte:head>
