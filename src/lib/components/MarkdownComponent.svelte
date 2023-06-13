c3f6e44043b07d85606d388a1d091d1a45d06999
Silabear <justsilas@yahoo.co.uk>
2023-06-13 19:13:31 +0100

    there is no freaking way this works

--- src/lib/components/MarkdownComponent.svelte
+++ src/lib/components/MarkdownComponent.svelte
@@ -10,12 +10,12 @@
 
   onMount(async () => {
     if (browser) {
-      await import { markedSmartypants } from "marked-smartypants";
-      await import { marked } from "marked";
-      marked.use(markedSmartypants());
+      const msp =  import("marked-smartypants");
+      const marked = await import("marked")
+      marked.marked.use(msp.markedSmartypants());
       if (!source) html = "";
       else {
-        html = await marked.parse(
+        html = await marked.marked.parse(
           DOMPurify.sanitize(source, {
             FORBID_ATTR: ["style", "class", "placeholder", "src"],
             FORBID_TAGS: ["canvas", "svg", "iframe", "img"],
