9503ca9cfb33887605f54b3aa44945bbd0d8c010
Silabear <justsilas@yahoo.co.uk>
2023-06-13 16:13:31 +0100

    change the orang

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,9 +10,6 @@
   import NProgress from 'nprogress'
   import {navigating} from '$app/stores'
 
-  // NProgress css
-  import 'nprogress/nprogress.css'
-
   NProgress.configure({
         // Full list: https://github.com/rstacruz/nprogress#configuration
         minimum: 0.16,
