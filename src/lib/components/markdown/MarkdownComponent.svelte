2a8297af48f968e5ed1dff7417d56c97538dba13
boop-the-dog <aidenhperry@gmail.com>
2023-06-26 11:21:26 -0400

    possible markdown fixes

--- src/lib/components/markdown/MarkdownComponent.svelte
+++ src/lib/components/markdown/MarkdownComponent.svelte
@@ -14,16 +14,19 @@
 
     const renderer = new marked.Renderer();
 
-    renderer.link = (href, title, text) => {
+    renderer.link = (href, _, text) => {
       return `<a href="https://datapackhub.net/linkout?url=${href}" target="_blank">${text}</a>`;
     };
 
     return marked.marked(
-      DOMPurify.sanitize(str, {
-        FORBID_ATTR: ["style", "class", "placeholder", "src"],
-        FORBID_TAGS: ["canvas", "svg", "iframe"],
-        ALLOWED_TAGS: ["details", "summary"]
-      }),
+      DOMPurify.sanitize(
+        str.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/, ""),
+        {
+          FORBID_ATTR: ["style", "class", "placeholder", "src"],
+          FORBID_TAGS: ["canvas", "svg", "iframe"],
+          ALLOWED_TAGS: ["details", "summary"]
+        }
+      ),
       {
         renderer: renderer,
         async: true,
@@ -65,7 +68,8 @@
   });
 </script>
 
-<div class="prose dark:prose-invert leading-snug break-words prose-table:bg-pearl-lusta-300 prose-table:dark:bg-stone-800 prose-td:p-2 prose-th:p-2 prose-table:rounded-md prose-tbody:odd:bg-stone-600 {classes}">
+<div
+  class="prose dark:prose-invert leading-snug break-words prose-table:bg-pearl-lusta-300 prose-table:dark:bg-stone-800 prose-td:p-2 prose-th:p-2 prose-table:rounded-md prose-tbody:odd:bg-stone-600 {classes}">
   <!-- eslint-disable-next-line svelte/no-at-html-tags -->
   {@html html}
 </div>
