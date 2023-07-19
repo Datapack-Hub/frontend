541efb38a0282ce6818e94d2909f62a796f98c00
boop-the-dog <aidenhperry@gmail.com>
2023-07-18 23:10:56 -0400

    why did this not add

---
+++ tests/notifications.test.ts
@@ -0,0 +1,82 @@
+import { expect, test } from "@playwright/test";
+import { config } from "dotenv";
+
+
+test.beforeAll(() => {
+  config();
+})
+
+test.beforeEach(async ({ page }) => {
+  const token = process.env.UNLIGHTHOUSE_DPH_TOKEN;
+  if (token) {
+    await page.context().addCookies([
+      {
+        name: "dph_token",
+        value: token,
+        url: "https://localhost:4173/",
+      },
+    ]);
+    await page.reload();
+  }
+});
+
+test("notifications can be removed", async ({ page }) => {
+  // check if it sent
+  await page.goto("/notifications");
+
+
+  // attempts to match "https://api.datapackhub.net/notifs" with no sub-routes
+  const res = await page.waitForResponse(async (res) => {
+    const parts = res.url().split("/")
+    return parts[3].includes("notifs")
+  })
+
+  // skip when no notifs exist
+  if((await res.json()).result.length == 0) {
+    test.skip()
+  }
+
+  for (let i = 0; i < (await page.locator(".testNotif").count()); i++) {
+    await page.locator(".closeButton").first().click();
+    const res = await page.waitForResponse((res) => {
+      return res.url().includes("/notifs/delete/");
+    });
+    expect(res.ok).toBeTruthy()
+  }
+  expect(await page.locator(".testNotif").count()).toEqual(0);
+});
+
+test("notification can be sent", async ({ page }) => {
+  // goto page
+  await page.goto("/user/hoodierocks/");
+
+  await test.step("send notification", async () => {
+
+    const notifBtn = page.locator("#send_notif")
+    await notifBtn.waitFor();
+    await notifBtn.click();
+
+    await page.locator("#notif-message").waitFor();
+    await page.locator("#notif-message").type("Test Test 123 **123** ~~123~~");
+
+    await page.locator("#notif-content").type("Test Test 123 **123** ~~123~~");
+    // select type
+    await page.locator("#notif-type").selectOption("announcement");
+
+    // send it!
+    await page.locator("#send_notif_btn").click();
+    const res = await page.waitForResponse((res) => {
+      return res.url().includes("/notifs/send/");
+    }, { timeout: 1500 });
+
+    expect(res.ok).toBeTruthy()
+  });
+
+  await test.step("go to page", async () => {
+    // check if it sent
+    await page.goto("/notifications");
+    await page.locator(".testNotif").first().waitFor();
+  });
+
+  expect(await page.locator(".testNotif").count()).toBeGreaterThan(0);
+});
