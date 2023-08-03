ae84ce819fa0be9458e513133742b4dbaa83c45b
boop-the-dog <aidenhperry@gmail.com>
2023-08-03 10:10:42 -0400

    aaa

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,14 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+  testDir: 'tests',
+  fullyParallel: true,
+  retries: process.env.CI ? 2 : 0,
 	webServer: {
 		command: 'pnpm build && pnpm preview',
     port: 4173,
     reuseExistingServer: !process.env.CI
-  },
-  testDir: 'tests'
+  }
 };
 
 export default config;
