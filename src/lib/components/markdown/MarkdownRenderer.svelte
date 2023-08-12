9f3ad46a624a7e2063f6849e4b475a0543ae132e
boop-the-dog <aidenhperry@gmail.com>
2023-08-12 12:40:55 -0400

    readd breaks

--- src/lib/components/markdown/MarkdownRenderer.svelte
+++ src/lib/components/markdown/MarkdownRenderer.svelte
@@ -71,7 +71,7 @@
 </script>
 
 <div
-  class="prose dark:prose-invert leading-snug break-words prose-table:bg-slate-300 prose-table:dark:bg-zinc-800 prose-td:p-2 prose-th:p-2 prose-table:rounded-md prose-code:before:content-none prose-code:after:content-none prose-tbody:odd:bg-zinc-600 {classes}">
+  class="prose dark:prose-invert leading-snug break-words prose-a:break-all prose-table:bg-slate-300 prose-table:dark:bg-zinc-800 prose-td:p-2 prose-th:p-2 prose-table:rounded-md prose-code:before:content-none prose-code:after:content-none prose-tbody:odd:bg-zinc-600 {classes}">
   <!-- eslint-disable-next-line svelte/no-at-html-tags -->
   {@html html}
 </div>
