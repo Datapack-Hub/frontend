f40c1779b4f47491c1226eb15e444e6005c78772
Aiden Perry <aidenhperry@gmail.com>
2023-08-13 18:41:31 -0400

    add delete all button that will hopefully work with no problems whatsoever

--- src/routes/notifications/+page.svelte
+++ src/routes/notifications/+page.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
-  import type { PageData } from "./$types";
   import Notification from "$lib/components/Notification.svelte";
+  import Button from "$lib/components/decorative/Button.svelte";
+  import { fetchAuthed } from "$lib/globals/functions";
   import autoAnimate from "@formkit/auto-animate";
-  import { onMount } from "svelte";
+  import type { PageData } from "./$types";
 
   export let data: PageData;
   let localNotifCopy = data.notifications;
-  let visible = false;
 
-  onMount(() => (visible = true));
+  function deleteAll() {
+    localNotifCopy.forEach(n => {
+      fetchAuthed("DELETE", `/notifs/delete/${n?.id}`)
+    })
+    localNotifCopy = []
+  }
 </script>
 
 <svelte:head>
@@ -20,12 +25,14 @@
   id="main-content"
   class="bg-slate-50 transition-all dark:bg-zinc-900 h-full px-4 sm:px-8 md:px-16 lg:px-24">
   <div class="flex-col items-center md:flex-row md:items-start">
-    <h1
-      class="pt-24 pb-2 text-center text-5xl font-bold text-slate-950 dark:text-white md:text-start md:text-5xl lg:text-6xl">
-      Notifications
-    </h1>
+    <div class="flex flex-col md:flex-row items-center justify-between pt-24 pb-6">
+      <h1
+        class="pb-2 text-center text-5xl font-bold text-slate-950 dark:text-white md:text-start md:text-5xl lg:text-6xl">
+        Notifications
+      </h1>
+      <Button click={deleteAll}>Remove All Notifications</Button>
+    </div>
     <div use:autoAnimate>
-      {#if visible}
         {#if data.notifications?.length == 0}
           <div class="flex items-center justify-center w-full h-96">
             <h1 class="text-xl text-slate-950 dark:text-white">
@@ -38,7 +45,6 @@
             <Notification {notification} />
           {/each}
         </ul>
-      {/if}
     </div>
   </div>
 </main>
