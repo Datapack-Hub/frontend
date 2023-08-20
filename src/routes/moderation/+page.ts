127753fdc1c658ff6d7200d9fe024b6aa934ddd6
HoodieRocks <HoodieRocks@users.noreply.github.com>
2023-08-20 14:51:36 +0000

    Apply formatting

--- src/routes/moderation/+page.ts
+++ src/routes/moderation/+page.ts
@@ -1,9 +1,9 @@
+import { browser } from "$app/environment";
 import { goto } from "$app/navigation";
 import { moderatorOrAbove } from "$lib/globals/functions";
 import { roleInfo } from "$lib/globals/stores";
 import { get } from "svelte/store";
 import type { PageLoad } from "./$types";
-import { browser } from "$app/environment";
 
 export const load = (async () => {
   if (browser && !moderatorOrAbove(get(roleInfo))) {
