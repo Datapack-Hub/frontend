f37ee9738bbad8e84fcb96ae88f06398882f2f9c
Silabear <56885288+Silabear@users.noreply.github.com>
2023-08-31 20:24:40 +0100

    optimised coming soon page and added info

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -57,38 +57,25 @@
       <Footer />
     {:else}
       <div class="fixed left-0 top-0 flex h-screen w-screen bg-zinc-900">
-        <div
-          class="relative m-auto w-2/3 justify-self-center rounded-xl bg-zinc-800 p-3 align-middle shadow-md shadow-black/25 md:w-1/2 lg:w-1/3">
-          <div class="flex items-center justify-center">
-            <img
-              src="/logos/dph.svg"
-              alt="logo"
-              class="mr-1 h-8 rounded-full transition-all hover:brightness-75"
-              height="32"
-              width="32" />
-            <span
-              class="hidden text-2xl font-bold text-white transition-colors md:block">
-              Datapack Hub
-            </span>
+        <div class="flex justify-around relative m-auto justify-self-center align-middle">
+          <div class="w-4/5 md:w-2/3 lg:w-3/4 space-y-2">
+            <div class="rounded-xl bg-zinc-800 p-3 shadow-md shadow-black/25  flex space-x-3">
+              <img src="/logos/dph.svg" class="w-1/4" />
+              <div>
+                <h1 class="text-3xl dark:text-white font-bold mb-1">Welcome to Datapack Hub!</h1>
+                <p class="text-slate-200 mb-1">Datapack Hub started in January 2022 as a PMC group for datapack developers to come together, forming a community around Minecraft Datapacks, Resource Packs, and more. Now, we’re a fast-growing community with over 500 followers, and we’re working on even more epic stuff. We strive to be the friendliest and best datapacking community out there. Thanks for checking us out!
+                <p class="text-slate-200 mt-2">Our website is still in development. In the meantime, come join our community!
+                </p>
+                <ul class="list-disc list-inside">
+                  <li><a href="https://discord.datapackhub.net" class="text-blue-400 font-semibold">Join our Discord (the main part of our project)</a></li>
+                  <li><a href="https://discord.datapackhub.net" class="text-blue-400">Follow our Planet Minecraft group</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="rounded-xl bg-red-800 p-3 shadow-md shadow-black/25 flex space-x-3">
+              <p class="dark:text-white"><b>The Datapack Hub Website is coming in Autumn. </b>If you are part of the website's closed beta and aren't meant to be seeing this page, please sign in again.</p>
+            </div>
           </div>
-          <CasualLine />
-          <p class="mb-5 text-lg text-white">
-            The Datapack Hub website is coming very soon!
-          </p>
-          <p class=" text-white"><b>In the meantime, come chat!</b></p>
-          <p>
-            <a href="https://discord.datapackhub.net" class=" text-sky-200">
-              - Join our Discord</a>
-          </p>
-          <p class="mb-5">
-            <a
-              href="https://planetminecraft.com/group/daily_datapackers"
-              class=" text-sky-200">
-              - Follow our PMC group!</a>
-          </p>
-          <p class=" text-sm font-thin italic text-white">
-            ~ your favourite datapack hub staff team
-          </p>
         </div>
       </div>
     {/if}
