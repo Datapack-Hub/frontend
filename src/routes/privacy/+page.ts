48b35a0d9bade794344647551802d776e8a4629f
boop-the-dog <aidenhperry@gmail.com>
2023-09-01 11:32:27 -0400

    prerendering, hopefully increases performance more?

--- src/routes/privacy/+page.ts
+++ src/routes/privacy/+page.ts
@@ -1,5 +1,7 @@
 import type { PageLoad } from "./$types";
 
+export const prerender = true
+
 export const load = (async ({ fetch }) => {
   const response = await fetch(
     "https://gist.githubusercontent.com/Silabear/2fae770ffd59b563668f68dafaa90b82/raw/d9d1ed01a772d4906495982fc6cfc707b93adf6b/policy.md"
