4c3360fa2a011e7f5593f05752c243406208495a
HoodieRocks <HoodieRocks@users.noreply.github.com>
2023-09-01 20:40:09 +0000

    Apply formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -52,7 +52,7 @@
 
 <div class="{$isDark ? 'dark' : ''} font-brand">
   {#if !maintenance || $user.role == "admin"}
-    {#if ($authed && ["admin", "moderator", "helper", "beta access", "verified", "nerd"].includes($user.role))}
+    {#if $authed && ["admin", "moderator", "helper", "beta access", "verified", "nerd"].includes($user.role)}
       <div class="min-h-screen bg-slate-50 transition-all dark:bg-zinc-900">
         {#if $user.banned}
           <BannedModal />
@@ -71,13 +71,14 @@
           class="relative m-auto flex justify-around justify-self-center align-middle">
           <div class="w-4/5 space-y-2 md:w-2/3 lg:w-3/4">
             <div
-              class="flex flex-col md:flex-row items-center justify-center md:justify-between space-x-3 rounded-xl bg-zinc-800 p-3 shadow-md shadow-black/25">
+              class="flex flex-col items-center justify-center space-x-3 rounded-xl bg-zinc-800 p-3 shadow-md shadow-black/25 md:flex-row md:justify-between">
               <img alt="Datapack Hub logo" src="/logos/dph.svg" class="w-1/4" />
               <div>
-                <h1 class="mb-1 text-xl md:text-2xl lg:text-3xl text-center md:text-left font-bold dark:text-white">
+                <h1
+                  class="mb-1 text-center text-xl font-bold dark:text-white md:text-left md:text-2xl lg:text-3xl">
                   Welcome to Datapack Hub!
                 </h1>
-                <p class="text-xs md:text-sm lg:text-base mb-1 text-slate-200">
+                <p class="mb-1 text-xs text-slate-200 md:text-sm lg:text-base">
                   Datapack Hub started in January 2022 as a PMC group for
                   datapack developers to come together, forming a community
                   around Minecraft Datapacks, Resource Packs, and more. Now,
@@ -94,13 +95,13 @@
                   <li>
                     <a
                       href="https://discord.datapackhub.net"
-                      class="text-xs md:text-sm lg:text-base font-semibold text-blue-400"
+                      class="text-xs font-semibold text-blue-400 md:text-sm lg:text-base"
                       >Join our Discord (the main part of our project)</a>
                   </li>
                   <li>
                     <a
                       href="https://discord.datapackhub.net"
-                      class="text-xs md:text-sm lg:text-base text-blue-400"
+                      class="text-xs text-blue-400 md:text-sm lg:text-base"
                       >Follow our Planet Minecraft group</a>
                   </li>
                 </ul>
@@ -108,7 +109,7 @@
             </div>
             <div
               class="flex space-x-3 rounded-xl bg-red-800 p-3 shadow-md shadow-black/25">
-              <p class="text-xs md:text-sm lg:text-base dark:text-white">
+              <p class="text-xs dark:text-white md:text-sm lg:text-base">
                 <b>The Datapack Hub Website is coming in Autumn. </b>If you are
                 part of the website's closed beta and aren't meant to be seeing
                 this page, please sign in again.
