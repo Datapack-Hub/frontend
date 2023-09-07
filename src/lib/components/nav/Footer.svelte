9c43001f6d0aa8e70f3271e87dee58723e545662
Aiden Perry <aidenhperry@gmail.com>
2023-09-07 18:17:49 -0400

    revert footer?

--- src/lib/components/nav/Footer.svelte
+++ src/lib/components/nav/Footer.svelte
@@ -1,47 +1,84 @@
-<footer class="border-t border-zinc-700 bg-zinc-900 pb-24">
-  <div class="mx-8 mt-16 flex items-center md:mx-16 md:mt-32">
-    <img src="/logos/dph.svg" alt="Datapack Hub" class="mr-1 h-8 w-8" />
-    <p class="text-xl font-bold text-white">Datapack Hub</p>
+<footer
+  class="flex h-[30rem] flex-col items-center justify-evenly border-t-2 border-slate-50 bg-slate-50 px-8 dark:border-zinc-700 dark:bg-zinc-900 md:px-16 lg:h-64 lg:flex-row lg:justify-between lg:px-24"
+  id="footer">
+  <div>
+    <div class="flex items-center space-x-2">
+      <img
+        loading="lazy"
+        src="/logos/dph.svg"
+        alt="logo"
+        height="48"
+        width="48"
+        class="h-12 w-full lg:w-auto" />
+      <p
+        class="hidden w-full text-center text-2xl font-bold text-slate-950 dark:text-white lg:block lg:text-left">
+        Datapack Hub
+      </p>
+    </div>
+    <p
+      class="mt-4 text-center text-sm font-light text-slate-950/50 dark:text-white/25 lg:text-left">
+      Copyright © 2023 Datapack Hub<br />Not an official Minecraft product. Not
+      approved by or associated with Mojang Studios
+    </p>
   </div>
-  <p class="ml-8 mt-3 text-sm text-zinc-600 md:ml-16">
-    Copyright 2023 © Datapack Hub<br />Not an official Minecraft product. Not
-    approved by or associated with Mojang Studios
-  </p>
-  <div
-    class="mx-8 mt-8 grid grid-cols-2 gap-3 text-white md:mx-16 md:mt-16 lg:grid-cols-4">
-    <div class="flex flex-col">
-      <h1 class="mb-3 mt-6 w-1/4 text-lg font-bold text-dph-orange">Social</h1>
-      <a class="footer_item" href="https://discord.datapackhub.net/">Discord</a>
-      <a class="footer_item" href="https://github.com/Datapack-Hub">GitHub</a>
-      <a class="footer_item" href="https://x.com/Datapack-Hub">X/Twitter</a>
-    </div>
-    <div class="flex flex-col">
-      <h1 class="mb-3 mt-6 w-1/4 text-lg font-bold text-dph-orange">Support</h1>
-      <a class="footer_item" href="/install">Install</a>
-      <a class="footer_item" href="/settings">Settings</a>
-      <a class="footer_item" href="/staff">Staff</a>
-    </div>
-    <div class="flex flex-col">
-      <h1 class="mb-3 mt-6 w-1/4 text-lg font-bold text-dph-orange">
-        Community
-      </h1>
-      <a class="footer_item" href="/jam">Jam</a>
-      <a class="footer_item" href="/login">Log in</a>
-      <a class="footer_item" href="/notifications">Notifications</a>
-    </div>
-    <div class="flex flex-col">
-      <h1 class="mb-3 mt-6 w-1/4 text-lg font-bold text-dph-orange">
+  <div class="hidden lg:block"></div>
+  <div class="w-full md:w-1/2">
+    <div class="flex w-full text-center">
+      <p class="mb-4 w-1/3 text-center text-xl font-bold text-dph-orange">
+        Support
+      </p>
+      <p class="mb-4 w-1/3 text-center text-xl font-bold text-dph-orange">
+        Socials
+      </p>
+      <p class="mb-4 w-1/3 text-center text-xl font-bold text-dph-orange">
         Policies
-      </h1>
-      <a class="footer_item" href="/staff">Staff</a>
-      <a class="footer_item" href="/privacy">Privacy</a>
-      <a class="footer_item" href="/rules">Rules</a>
+      </p>
+    </div>
+    <!-- put header content here -->
+    <div class="flex w-full text-center">
+      <ul class="w-1/3 text-center text-slate-950 dark:text-white">
+        <li>
+          <a
+            class="transition-transform hover:brightness-75"
+            href="https://discord.datapackhub.net">Community Discord</a>
+        </li>
+        <li>
+          <a class="transition-transform hover:brightness-75" href="/staff"
+            >Staff Team</a>
+        </li>
+        <li>
+          <a class="transition-transform hover:brightness-75" href="/staff"
+            >About us</a>
+        </li>
+      </ul>
+      <ul class="w-1/3 text-center text-slate-950 dark:text-white">
+        <li>
+          <a
+            class="transition-transform hover:brightness-75"
+            href="https://discord.datapackhub.net">Discord</a>
+        </li>
+        <li>
+          <a
+            class="transition-transform hover:brightness-75"
+            href="https://github.com/Datapack-Hub">GitHub</a>
+        </li>
+        <li>
+          <a
+            class="transition-transform hover:brightness-75"
+            href="https://twitter.com/DatapackHub">Twitter</a>
+        </li>
+      </ul>
+      <ul class="w-1/3 text-center text-slate-950 dark:text-white">
+        <li>
+          <a class="transition-transform hover:brightness-75" href="/rules"
+            >Rules</a>
+        </li>
+        <li>
+          <a class="transition-transform hover:brightness-75" href="/privacy"
+            >Privacy</a>
+        </li>
+      </ul>
     </div>
   </div>
+  <div class="py-16"></div>
 </footer>
-
-<style lang="postcss">
-  .footer_item {
-    @apply text-sm font-light;
-  }
-</style>
