9a54cb3cf22db1815247c8531713e1988a1982ab
Aiden Perry <aidenhperry@gmail.com>
2023-10-21 09:57:50 -0400

    change linkout page more

--- src/routes/linkout/+page.svelte
+++ src/routes/linkout/+page.svelte
@@ -7,11 +7,11 @@
 
 <main
   id="main-content"
-  class="flex h-screen flex-col md:flex-row items-center justify-center md:justify-between px-4 sm:px-8 md:px-16 lg:px-24">
-  <img src="/logos/dph_stop.svg" alt="logo" class="w-full sm:w-1/2 md:w-1/4 p-12" />
-  <div class="w-3/4">
+  class="flex h-screen flex-col items-center justify-center space-x-0 px-4 sm:px-8 md:flex-row md:space-x-8 md:px-16 lg:space-x-24 lg:px-24 xl:space-x-48">
+  <img src="/logos/dph_stop.svg" alt="logo" class="w-48" />
+  <div class="w-full md:w-2/3">
     <h1
-      class="mb-4 text-3xl font-bold text-zinc-950 dark:text-zinc-100 md:text-4xl lg:text-5xl">
+      class="mb-4 text-4xl font-bold text-zinc-950 dark:text-zinc-100 md:text-4xl lg:text-5xl">
       Stop!
     </h1>
     <p class="text-base text-zinc-950 dark:text-zinc-100 md:text-lg lg:text-xl">
@@ -20,15 +20,16 @@
       safe before continuing!
     </p>
     <p
-      class="text-sm text-zinc-950/60 dark:text-zinc-100/60 md:text-base lg:text-lg">
+      class="mt-2 text-sm text-zinc-950/60 dark:text-zinc-100/60 md:text-base lg:text-lg">
       URL: {data.url}
     </p>
-    <div class="mt-4 flex flex-col space-y-1 md:flex-row w-full justify-between md:w-1/2 lg:w-2/5">
+    <div
+      class="mt-4 flex flex-col space-x-0 space-y-3 md:flex-row md:space-x-4 md:space-y-0">
       <a
-        class="button-primary"
+        class="button-primary text-center"
         rel="nofollow"
         href="{decodeURIComponent(data.url ?? 'https://datapackhub.net')}"
-        >Continue to page</a>
+        >Continue to site</a>
       <Button click="{() => history.back()}">Go Back</Button>
     </div>
   </div>
