dfdaa0a6a4ab935c2f542d7755656c28949dd383
Cobblestone <aidenhperry@gmail.com>
2023-11-30 07:29:34 -0500

    boo

--- src/lib/components/CookieConsent.svelte
+++ src/lib/components/CookieConsent.svelte
@@ -24,12 +24,12 @@
 <div
   class="fixed z-50 {$windowWidth > 768
     ? 'bottom-0'
-    : 'top-0'} left-0 grid w-full grid-cols-2 rounded-lg bg-white p-3">
+    : 'top-0'} left-0 grid w-full grid-cols-2 rounded-lg bg-white dark:bg-black p-3">
   <div>
-    <p class="text-base font-bold md:text-lg lg:text-xl">
+    <p class="text-base dark:text-white font-bold md:text-lg lg:text-xl">
       Hey there, we've got some news—we use cookies on our site.
     </p>
-    <p class="text-xs md:text-sm lg:text-base">
+    <p class="text-xs dark:text-white md:text-sm lg:text-base">
       They make this site tick. If you're cool with that, awesome—hit 'Accept.'
       Not your thing? That's cool too, hit 'Decline.' Check out our <a
         href="/privacy"
@@ -37,11 +37,11 @@
     </p>
   </div>
   <div class="flex items-center justify-end gap-3">
-    <button class="button-alt" on:click="{() => acceptCookies()}"
+    <button class="button-secondary" on:click="{() => acceptCookies()}"
       >Accept</button>
-    <button class="button-alt" on:click="{() => denyCookies()}">Decline</button>
+    <button class="button-secondary" on:click="{() => denyCookies()}">Decline</button>
     {#if $windowWidth > 768}
-      <a href="/privacy" class="button-alt">Privacy Policy</a>
+      <a href="/privacy" class="button-secondary">Privacy Policy</a>
     {/if}
   </div>
 </div>
