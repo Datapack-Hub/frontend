4b0aa8121dc2995037b7ddfaba06388fd84471ec
Cobblestone <aidenhperry@gmail.com>
2023-12-03 17:44:49 -0500

    why u breakin

--- src/lib/globals/consts.ts
+++ src/lib/globals/consts.ts
@@ -46,17 +46,17 @@ export const minecraftVersions = [
 ];
 
 export const dpPackFormats = [
-    {"format":4,"label":"1.13"},
-    {"format":5,"label":"1.15"},
-    {"format":6,"label":"1.16.2"},
-    {"format":7,"label":"1.17"},
-    {"format":8,"label":"1.18"},
-    {"format":9,"label":"1.18.2"},
-    {"format":10,"label":"1.19"},
-    {"format":12,"label":"1.19.4"},
-    {"format":15,"label":"1.20"},
-    {"format":18,"label":"1.20.2"},
-    {"format":26,"label":"1.20.3-pre1"},
+  { format: 4, label: "1.13" },
+  { format: 5, label: "1.15" },
+  { format: 6, label: "1.16.2" },
+  { format: 7, label: "1.17" },
+  { format: 8, label: "1.18" },
+  { format: 9, label: "1.18.2" },
+  { format: 10, label: "1.19" },
+  { format: 12, label: "1.19.4" },
+  { format: 15, label: "1.20" },
+  { format: 18, label: "1.20.2" },
+  { format: 26, label: "1.20.3-pre1" }
 ];
 
 export type FeaturedProjectTypes =
