56bb3f18af006d159866e66acfa79dd443d911bd
Silabear <56885288+Silabear@users.noreply.github.com>
2023-12-22 13:00:27 +0000

    remove barricade

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -35,7 +35,6 @@
 
 <div class="font-brand {$isDark ? 'dark' : ''}">
   {#if !maintenance || $user.role == "admin"}
-    {#if dev || ($authed && ["admin", "moderator", "helper", "beta access", "verified", "nerd"].includes($user.role))}
       <div class="min-h-screen bg-slate-50 transition-all dark:bg-zinc-900">
         {#if $user.banned}
           <BannedModal />
@@ -51,60 +50,6 @@
         position="{$windowWidth > 768 ? 'bottom-right' : 'top-center'}"
         richColors />
       <Footer />
-    {:else}
-      <div class="fixed left-0 top-0 flex h-screen w-screen bg-zinc-900">
-        <div
-          class="relative m-auto flex justify-around justify-self-center align-middle">
-          <div class="w-4/5 space-y-2 md:w-2/3 lg:w-3/4">
-            <div
-              class="flex flex-col items-center justify-center space-x-3 rounded-xl bg-zinc-800 p-3 shadow-md shadow-black/25 md:flex-row md:justify-between">
-              <img alt="Datapack Hub logo" src="/logos/dph.svg" class="w-1/4" />
-              <div>
-                <h1
-                  class="mb-1 text-center text-xl font-bold md:text-left md:text-2xl lg:text-3xl dark:text-white">
-                  Welcome to Datapack Hub!
-                </h1>
-                <p class="mb-1 text-xs text-zinc-200 md:text-sm lg:text-base">
-                  Datapack Hub started in January 2022 as a PMC group for
-                  datapack developers to come together, forming a community
-                  around Minecraft Datapacks, Resource Packs, and more. Now,
-                  we're a fast-growing community with over 500 followers, and
-                  we're working on even more epic stuff. We strive to be the
-                  friendliest and best datapacking community out there. Thanks
-                  for checking us out!
-                </p>
-                <p class="mt-2 text-zinc-200">
-                  Our website is still in development. In the meantime, come
-                  join our community!
-                </p>
-                <ul class="list-inside list-disc">
-                  <li>
-                    <a
-                      href="https://discord.datapackhub.net"
-                      class="text-xs font-semibold text-blue-400 md:text-sm lg:text-base"
-                      >Join our Discord (the main part of our project)</a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://discord.datapackhub.net"
-                      class="text-xs text-blue-400 md:text-sm lg:text-base"
-                      >Follow our Planet Minecraft group</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div
-              class="flex space-x-3 rounded-xl bg-red-800 p-3 shadow-md shadow-black/25">
-              <p class="text-xs md:text-sm lg:text-base dark:text-white">
-                <b>The Datapack Hub Website is coming in Autumn. </b>If you are
-                part of the website's closed beta and aren't meant to be seeing
-                this page, please sign in again.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    {/if}
   {:else}
     <div
       class="fixed left-0 top-0 flex h-screen w-screen bg-zinc-900 align-middle">
