0588cefbfe72079352e4f075bf5bda4820834557
PuckiSilver <pucki.silver@gmail.com>
2023-12-22 16:24:14 +0100

    add 1.20.4 hopefully

--- src/lib/globals/consts.ts
+++ src/lib/globals/consts.ts
@@ -44,7 +44,8 @@ export const minecraftVersions = [
   "1.19",
   "1.19.4",
   "1.20",
-  "1.20.2"
+  "1.20.2",
+  "1.20.4"
 ];
 
 export const dpPackFormats = [
@@ -58,7 +59,8 @@ export const dpPackFormats = [
   { format: 12, label: "1.19.4" },
   { format: 15, label: "1.20" },
   { format: 18, label: "1.20.2" },
-  { format: 26, label: "1.20.3-pre1" }
+  { format: 26, label: "1.20.3-pre1" },
+  { format: 26, label: "1.20.4" }
 ];
 
 export type FeaturedProjectTypes =
