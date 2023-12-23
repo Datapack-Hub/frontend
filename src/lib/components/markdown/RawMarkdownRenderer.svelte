cff513fa954558710686dd5fab0494bc742fc917
HoodieRocks <HoodieRocks@users.noreply.github.com>
2023-12-23 22:08:45 +0000

    yo code so ugly the bot had to fix it 💀

--- src/lib/components/markdown/RawMarkdownRenderer.svelte
+++ src/lib/components/markdown/RawMarkdownRenderer.svelte
@@ -6,17 +6,17 @@
 <!-- tailwind is so clean ._. -->
 <div
   class="prose break-words
-  dark:prose-invert prose-a:break-all
+  dark:prose-invert prose-p:my-3
+  prose-a:break-all
   prose-a:underline
-  prose-p:my-3
-  prose-hr:my-12
   prose-blockquote:border-slate-300
-  prose-code:before:content-none
-  prose-code:after:content-none
-  prose-pre:p-3 prose-pre:bg-slate-300 dark:prose-pre:bg-zinc-950
-  prose-code:bg-slate-300 dark:prose-code:bg-zinc-950 prose-code:p-1 prose-code:rounded-md prose-table:rounded-lg
-  prose-table:bg-slate-300/40 prose-th:p-3
-  prose-td:p-3 prose-img:py-3 prose-blockquote:dark:border-zinc-600
+  prose-code:rounded-md
+  prose-code:bg-slate-300
+  prose-code:p-1
+  prose-code:before:content-none prose-code:after:content-none prose-pre:bg-slate-300
+  prose-pre:p-3 prose-table:rounded-lg prose-table:bg-slate-300/40 prose-th:p-3 prose-td:p-3
+  prose-img:py-3 prose-hr:my-12
+  prose-blockquote:dark:border-zinc-600 dark:prose-code:bg-zinc-950 dark:prose-pre:bg-zinc-950
   prose-table:dark:bg-zinc-700/40 {classes}">
   <!-- eslint-disable-next-line svelte/no-at-html-tags -->
   {@html html}
