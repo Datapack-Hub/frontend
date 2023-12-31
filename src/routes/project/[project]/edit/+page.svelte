ef84daad709b4b8b6f0fc38001b233706a34e75f
HoodieRocks <HoodieRocks@users.noreply.github.com>
2023-12-31 00:30:50 +0000

    yo code so ugly the bot had to fix it 💀

--- src/routes/project/[project]/edit/+page.svelte
+++ src/routes/project/[project]/edit/+page.svelte
@@ -463,9 +463,7 @@
           <p class="col-span-3 text-zinc-950 dark:text-zinc-100">Categories</p>
           <div
             class="col-span-4 grid grid-cols-2 gap-3 rounded-lg md:grid-cols-3 lg:grid-cols-4">
-            <ToggleBoxes
-              options="{categories}"
-              bind:selected="{category}" />
+            <ToggleBoxes options="{categories}" bind:selected="{category}" />
           </div>
           <div class="col-span-3"></div>
           <button
