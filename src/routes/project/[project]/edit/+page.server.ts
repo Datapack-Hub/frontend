4276f6e09eb5e1829dc259599f32f125e99c4750
Cobblestone <aidenhperry@gmail.com>
2024-01-03 11:56:47 -0500

    fix image uploading ig

--- src/routes/project/[project]/edit/+page.server.ts
+++ src/routes/project/[project]/edit/+page.server.ts
@@ -84,12 +84,22 @@ export const load = (async event => {
   });
 }) satisfies PageServerLoad;
 
+function bufferToBase64(buffer: ArrayBuffer) {
+    let binary = "";
+    const bytes = new Uint8Array(buffer);
+    const length_ = bytes.byteLength;
+    for (let index = 0; index < length_; index++) {
+      binary += String.fromCodePoint(bytes[index]);
+    }
+    return btoa(binary);
+}
+
 async function blobToB64(buffer: Blob) {
   return (
     "data:" +
     buffer.type +
     ";base64," +
-    Buffer.from(await buffer.arrayBuffer()).toString("base64")
+    bufferToBase64(await buffer.arrayBuffer())
   );
 }
 
