6cbc9c42a8bacd1fca1161e5294ee9994613ce1e
HoodieRocks <HoodieRocks@users.noreply.github.com>
2024-01-23 21:20:53 +0000

    yo code so ugly the bot had to fix it 💀

--- src/lib/components/user/UserCard.svelte
+++ src/lib/components/user/UserCard.svelte
@@ -35,7 +35,7 @@
             width="18" />Datapack Hub Team
         {/if}
       </span>
-      <span class="max-h-72 line-clamp-2">
+      <span class="line-clamp-2 max-h-72">
         <RawMarkdownRenderer html="{person?.bio}" classes="{'line-clamp-2'}" />
       </span>
     </div>
